ab8272af73d7acd70c362109977ef6de7bda0f46
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-03-25 12:54:16 +0200

    Added add.js content

---
+++ 2_lecture/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+const App = () => {
+  return ( <div>
+  <h1>HeEELLOOO, World! its corona time</h1>
+  </div> 
+  );
+    
+}
+
+
+export default App;

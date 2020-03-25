f572e592958348edfad5452f5e898093a9b7939f
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-03-25 14:30:53 +0200

    2_lecture

--- 2_lecture/src/App.js
+++ 2_lecture/src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+class App extends React.Component{
+  state = {
+    persons: [
+      {name: 'Margit', age: 39},
+      {name: 'karim', age: 39},
+      {name: 'crisel', age: 39},
+      {name: 'kaisa', age: 39},
+      {name: 'liina', age: 39}
+    ]
+  };
 
-const App = () => {
-  return ( <div>
-  <h1>HeEELLOOO, World! its corona time</h1>
-  <Person />
-  </div> 
-  );
-    
+  render () {
+    return (
+      <div> 
+        <h1>Hello World</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      </div>
+    );
+  };
 };
+
+
 
 
 export default App;
--- 2_lecture/src/Person/Person.js
+++ 2_lecture/src/Person/Person.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './Person.css';
 
 class Person extends React.Component{
-    render() {
-    return <p>Hello, my name is {this.props.name}</p>
+    
+        render() {
+            return (<div className="card"> 
+    <p>Hello, my name is {this.props.name} and i'm {this.props.age} years old </p>
+    </div> );
     }
 }
+
 export default Person;
 
 

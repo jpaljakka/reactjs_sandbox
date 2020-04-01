1b584d5090d19dd4d0a9971c95f0ed153cdd4374
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-04-01 20:25:19 +0300

    asdasdasdas

---
+++ 4_lecture/src/View/View.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const View = (view) => {
+    
+    return (
+        <div>
+            <h1>This is your input</h1>
+            <p>Firstname: {view.name} </p>
+            
+        </div>
+        
+    )
+    
+}
+
+export default View

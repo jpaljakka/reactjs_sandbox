f5e3a514c78bffcd80897c120c6e540ea42d57c5
jpaljakka <55959622+jpaljakka@users.noreply.github.com>
2020-04-06 12:54:01 +0300

    speedtest styling

--- speed-test/src/App.js
+++ speed-test/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Component } from 'react';
+import './App.css';
+import Circle from './Circle/Circle'
+
+class App extends Component{
+
+render(){
+  return(
+    <>
+    <div className="container">
+    <div className="circleWrapper">
+    <header>
+      <h1>SPEEDTEST</h1>
+      <p>Your score:</p>
+        </header>
+    <Circle/>
+    <Circle/>
+    <Circle/>
+    <Circle/>
     </div>
-  );
+    <div>
+      
+  
+      <div>
+      <button> START </button>
+      <button> STOP </button>
+      </div>
+      </div>
+    </div>
+    </>
+    )
+}
+
 }
 
 export default App;
--- speed-test/src/Circle/Circle.js
+++ speed-test/src/Circle/Circle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import "./Circle.css"
+
+const Circle = (props) =>{
+     
+    return(
+<>
+        <button className="circles"></button>
+      
+</>
+
+    )
+    
+}
+
+export default Circle
